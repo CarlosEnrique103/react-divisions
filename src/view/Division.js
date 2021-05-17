6bf3c66b7cdac52ed0c73ec1865474e8fccc0a07
juancarlos <juancarlosenrique103@gmail.com>
2021-05-17 07:27:03 -0500

    Created view for routes

---
+++ src/view/Division.js
@@ -0,0 +1,14 @@
+import Main from '../components/Main';
+import Navbar from '../components/Navbar';
+import './Division.scss';
+
+function Division() {
+  return (
+    <div className="Divisions">
+      <Navbar />
+      <Main />
+    </div>
+  );
+}
+
+export default Division;
---
+++ src/view/Home.js
@@ -0,0 +1,16 @@
+import { useHistory } from "react-router-dom";
+import { Button } from "antd";
+import './Home.scss'
+
+function Home() {
+  const history = useHistory();
+  return (
+    <div className="Home">
+      <Button  onClick={() => history.push("organizacion")}>
+        Ir a Organización
+      </Button>
+    </div>
+  );
+}
+
+export default Home;
